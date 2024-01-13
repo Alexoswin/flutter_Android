922443d42cb876db617aa680c9eee86860e0decf
oswin <oswinalex1@gmail.com>
2024-01-14 01:35:56 +0530

    patterns in dart

--- dart/loops.dart
+++ dart/loops.dart
@@ -12,6 +12,73 @@ for(int i = 1; i<=size; i++){
     print('');
 }
 
+// tringle 2 
+print("");
+
+for(int i = 1; i<=size; i++){
+    int space = size - i;
+    for(int j = space ;j>=1; j--){
+        stdout.write("   ");
+    }
+    for(int j = 1 ;j<=i; j++){
+        stdout.write(" X ");
+    }
+    
+    print('');
+}
+
+// tringle 3 
+print("");
+
+for(int i = 1; i<=size; i++){
+    int space = size - i;
+    for(int j = space ;j>=1; j--){
+        stdout.write("   ");
+    }
+    for(int j = 1 ;j<=i; j++){
+        stdout.write(" X ");
+    }
+     for(int j = 1 ;j<=i-1; j++){
+        stdout.write(" X ");
+    }
+    print("");
+    
+}
+
+// diamond
+print("");
+
+for(int i = 1; i<=size; i++){
+    int space = size - i;
+    for(int j = space ;j>=1; j--){
+        stdout.write("   ");
+    }
+    for(int j = 1 ;j<=i; j++){
+        stdout.write(" X ");
+    }
+     for(int j = 1 ;j<=i-1; j++){
+        stdout.write(" X ");
+    }
+    print("");
+    
+}
+
+for(int i = size-1; i>=1; i--){
+    int space = size - i;
+    for(int j = space ;j>=1; j--){
+        stdout.write("   ");
+    }
+    for(int j = 1 ;j<=i; j++){
+        stdout.write(" X ");
+    }
+     for(int j = 1 ;j<=i-1; j++){
+        stdout.write(" X ");
+    }
+    print("");
+    
+}
+
+
 
 
 }
