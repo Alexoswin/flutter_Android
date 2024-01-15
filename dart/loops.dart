2f9a987717217a28df723829791aa10d291fbc48
oswin <oswinalex1@gmail.com>
2024-01-15 23:34:15 +0530

    patterns using loops in dart

--- dart/loops.dart
+++ dart/loops.dart
@@ -79,6 +79,36 @@ for(int i = size-1; i>=1; i--){
 }
 
 
+// to print a rectangle 
+print("enter the length of the rectangle");
+int length = int.parse(stdin.readLineSync()!);
+print("enter the breath of the rectangle");
+int breath = int.parse(stdin.readLineSync()!);
+
+
+for(int i = 1; i<=breath; i++){
+    for(int j = 1; j<=length; j++){
+        stdout.write(" X ");
+    }
+    print("");
+}
+
+print("");
+
+// hollow rectangle
+for(int i = 1;i<=breath;i++){
+    for(int j = 1;j<=length;j++){
+        if(i==1 || j==1 || i== breath || j== length){
+            stdout.write(" X ");
+        }
+        else{
+            stdout.write("   ");
+        }
+    }
+    print("");
+}
+
+
 
 
 }
