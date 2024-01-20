601c934627afdd8846d14766b283f7517b6af779
oswin <oswinalex1@gmail.com>
2024-01-20 17:43:52 +0530

    Stateless widget

--- demo_flutter/lib/main.dart
+++ demo_flutter/lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 int i = 0;
 void main() => runApp(MaterialApp(
-  home:Scaffold(
-    appBar: AppBar(
-      title: Text("hello world Oswin"),
-      backgroundColor: Colors.yellow,
-      centerTitle: true,
+  home: Home(),
+));
 
-    ),
+
+//state less widget
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hello world Oswin"),
+        backgroundColor: Colors.yellow,
+        centerTitle: true,
+
+
+      ),
       body: Center(
-        child: Text("hello world"),
+        child: Text("hello world  ${i}"  ),
 
       ),
 
       floatingActionButton: FloatingActionButton(
 
-       onPressed: (){
-         i= i+1;
-         print(i);
-       },
-        child: Text("Click"),
+        onPressed: (){
+          i= i+1;
+          print(i);
+
+        },
+        child: Text("Click ${i}"),
         backgroundColor: Colors.blue,
-  ) ,
+      ) ,
 
 
-  ),
-));
+    );
+  }
+}
+
